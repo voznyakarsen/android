76097aeafba7ec3337b942db95d8892979d52d17
voznyakarsen <120511872+voznyakarsen@users.noreply.github.com>
2024-01-03 19:59:08 +0200

    Create WheaterData.kt

---
+++ WheaterData.kt
@@ -0,0 +1,13 @@
+data class WeatherData(
+    val name: String,
+    val main: Main,
+    val weather: List<Weather>
+)
+
+data class Main(
+    val temp: Double
+)
+
+data class Weather(
+    val description: String
+)
